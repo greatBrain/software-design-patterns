8a7381e356f35921751b5c797b23934e59ad5acd
greatbrain <erickfania@gmail.com>
2024-11-12 16:04:08 -0400

    add:more real world project

---
+++ creational/factory_method/seedy_notifications_system/notifications/notification.py
@@ -0,0 +1,8 @@
+from abc import ABCMeta, ABC, abstractstaticmethod
+
+class INotification(metaclass=ABCMeta):
+      
+      @abstractstaticmethod
+      def send(self, message:str)->None:
+          ''' To be implemented by clients '''
+
