d1f48db90bf111bb3dfd55ca1acdb9b42952b42f
greatbrain <erickfania@gmail.com>
2024-11-23 07:42:15 -0400

    add:directory creation from file type

--- creational/factory_method/fileConverter/utils/helpers.py
+++ creational/factory_method/fileConverter/utils/helpers.py
@@ -12,19 +12,20 @@ class Helper:
          if not file_abs_path:
             raise ValueError("File not found")
          
-         self.file_path_and_extension = (file_abs_path.parent, Path(file_abs_path).suffix)         
-         self.dir_created(self.file_path_and_extension[1])
+         self.file_path_and_extension = (file_abs_path.parent, Path(file_abs_path).suffix.lstrip('.'))       
+         file_ext = self.file_path_and_extension[1]
+         self.dir_created(file_ext)
 
 
       def dir_created(self, file_type:str='')->bool:
-          new_dir_name = str(file_type)
+          new_dir_name = str(file_type + '_files')
           new_dir = Path(new_dir_name)
 
           try:
-            new_dir.mkdir(parents=True, exists_ok=True)
+            new_dir.mkdir(parents=True, exist_ok=True)
             return True 
-          except:
-            raise Exception("Directory could not be created", Exception)
+          except ValueError as e:
+            raise ("Directory could not be created", e)
 
 
       def save(self, src_path:str='', dst_path:str='')->bool:
