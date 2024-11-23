b2b99bbbd74a2206b82fdf303fd29b3998827575
greatbrain <erickfania@gmail.com>
2024-11-23 07:41:24 -0400

    feat:arguments now like options

--- creational/factory_method/fileConverter/cli.py
+++ creational/factory_method/fileConverter/cli.py
@@ -2,12 +2,18 @@ from utils.helpers import Helper
 import click
 
 @click.command()
-@click.argument('file', type=click.Path(exists=True))
-def run(file)->None:
+#@click.argument('file', type=click.Path(exists=True))
+@click.option('-f', '--file', type=click.Path(exists=True), help="The path of the file you want to convert.")
+@click.option('--convert', is_flag=True, help="The file must be converted.")
+def run(file, convert)->None:
     helper = Helper()
 
-    def _get_file_from_argument()-> None:
-        helper.get_file_name_and_extension(file)
-    _get_file_from_argument()
-
-    del helper
+    try:
+        if convert:
+           def _get_file_from_argument()-> None:
+               helper.get_file_name_and_extension(file)
+           _get_file_from_argument()        
+    except:
+        raise ValueError("No file or convertion option found")
+    finally:
+        del helper
