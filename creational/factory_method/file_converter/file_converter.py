4a9feb4b26b7aaa577caca7fdbf0b79addd8b1aa
greatbrain <erickfania@gmail.com>
2024-12-05 16:08:31 -0400

    feat: csv files to json files convertion

--- creational/factory_method/file_converter/file_converter.py
+++ creational/factory_method/file_converter/file_converter.py
@@ -3,12 +3,11 @@ import json
 from Iconverter import IConverter
 import helpers
 
-class CSVConverter(IConverter):
-      
+class CSVConverter(IConverter):      
       '''converter method implementation. Converts to json format.'''
       def convert(self, csv_file:str):          
           data = []
-          json_file = 'data.json'          
+          json_file = 'converted.json'          
           
           try:
               with open(csv_file, encoding='utf-8') as csvf:
@@ -16,8 +15,7 @@ class CSVConverter(IConverter):
                    data = [row for row in csv_reader]
 
               with open(json_file, 'w') as jf:
-                   json.dump(data, jf)  
-                                    
+                   json.dump(data, jf)                                    
               helpers.save_file(json_file, 'json')
                 
           except FileNotFoundError:
@@ -25,10 +23,11 @@ class CSVConverter(IConverter):
           
           except Exception as e:
                 print("An unexpected error occurred: {}".format(e))
+
+
             
             
 class JSONConverter(IConverter):
-      
       '''converter method implementation. Converts to csv format.'''
       def convert(self, _file:str):
           pass
--- creational/factory_method/file_converter/helpers.py
+++ creational/factory_method/file_converter/helpers.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import json
 
 
 # File validation:
@@ -6,17 +7,36 @@ def file_exists(_file:str)->bool:
     _file_path = Path(_file)    
     return True if _file_path.is_file() else False       
 
+
 def get_file_type(_file:str)->str:
     _file_path = Path(_file)
     _file_type = _file_path.suffix.lstrip('.')
     
     if not _file_type:
-       raise ValueError("The file {} does not have a valid extension.".format(_file))
-   
+       raise ValueError("The file {} does not have a valid extension.".format(_file))   
     return _file_type
 
 
-def save_file(new_file:str, file_type:str) -> bool:
-    directory = Path(file_type)    
-    directory.mkdir(parents=True, exist_ok=True)
+def save_file(new_file:str, file_type:str) -> bool:    
+    _file = Path(new_file)  #Path object with the file
+    destiny_dir = get_directory(file_type) # Name says it all
+    _file.rename(destiny_dir / _file.name) # Removes the file, created in the program namespace, to the desired dir       
+    
        
+def get_directory(file_type:str) -> str:    
+    dir_name:str = "{}_files".format(file_type)    
+    dir_path = Path(dir_name)
+    
+    if not dir_path.exists():
+       dir_path.mkdir(parents=True, exist_ok=True)
+       return dir_path
+    
+    return None
+
+
+def get_json_data(_file:str) -> json:
+    
+    # To normalize the  json file before convert to csv
+    with open(_file, "r") as jf:
+         j_data = json.load(jf)   
+    return j_data
